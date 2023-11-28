a16d15004433a1d4e450bfaaffc5adf1ef2a32da
Steve Fahy 9500 <stevefahydev@gmail.com>
2023-11-28 15:24:03 +0000

    Added Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,6 +26,23 @@ class myDocument extends Document {
           <meta name="emotion-insertion-point" content="" />
           {(this.props as any).emotionStyleTags}
         </Head>
+
+        <div className="container">
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-S8H3MZ1K57"
+          ></Script>
+          <Script id="google-analytics">
+            {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-S8H3MZ1K57');
+        `}
+          </Script>
+        </div>
+
         <body>
           <Main />
           <NextScript />
