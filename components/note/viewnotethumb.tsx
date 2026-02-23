26e014cff231efbfc6865edad6b967a1496bada7
Steve Fahy 9500 <stevefahydev@gmail.com>
2026-02-23 12:54:08 +0000

    Thumbnail previews now show only the first ~10 blocks of each note

--- components/note/viewnotethumb.tsx
+++ components/note/viewnotethumb.tsx
@@ -1,12 +1,14 @@
 import { memo } from "react";
 import dynamic from "next/dynamic";
 import matter from "gray-matter";
+import { truncateMarkdownPreview } from "../../lib/truncateMarkdownPreview";
 import classes from "./viewnotethumb.module.css";
 
 const ViewNoteMarkdown = dynamic(() => import("./viewnote_markdown"));
 
 const ViewNoteThumb = (props: any) => {
-  const { data, content } = matter(props.text);
+  const { content: rawContent } = matter(props.text);
+  const content = truncateMarkdownPreview(rawContent);
 
   const updateViewText = (a: any) => {
     props.updatedViewText(a);
