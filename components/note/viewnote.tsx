4be57ba2627b7b6bfd79d2645c3b04b27ec22e6c
Steve Fahy 9500 <stevefahydev@gmail.com>
2026-03-30 14:29:18 +0100

    Lint error fix: Avoid calling setState() directly within an effect

--- components/note/viewnote.tsx
+++ components/note/viewnote.tsx
@@ -1,27 +1,16 @@
-import { useState, useEffect, Fragment, memo } from "react";
+import { Fragment, memo } from "react";
 import matter from "../../lib/matter";
 import { NoteEditorView } from "../../types";
-import { SkeletonBlock } from "../ui/skeleton-block";
 import ViewNoteMarkdown from "./viewnote_markdown";
 
 const ViewNote = (props: NoteEditorView) => {
   const splitscreen = props.splitScreen;
 
   const { content } = matter(props.viewText);
-  const [contextView, setContextView] = useState("");
-  const [isLoaded, setIsLoaded] = useState(false);
 
   const updateViewText = (a: string | ((prev: string) => string)): void => {
     props.updatedViewText(a);
   };
-
-  useEffect(() => {
-    if (content !== contextView) {
-      setContextView(content);
-    }
-    setIsLoaded(true);
-    return () => {};
-  }, [content, contextView]);
 
   const viewPaneClassName = [
     "note-pane",
@@ -41,19 +30,12 @@ const ViewNote = (props: NoteEditorView) => {
               id="viewnote_id"
               className="v-card-text cardcontent viewnote_content"
             >
-              {!isLoaded ? (
-                <SkeletonBlock
-                  className="skeleton-view-placeholder"
-                  height={50}
-                />
-              ) : (
-                <ViewNoteMarkdown
-                  splitScreen={splitscreen}
-                  viewText={contextView}
-                  updatedViewText={updateViewText}
-                  disableLinks={false}
-                />
-              )}
+              <ViewNoteMarkdown
+                splitScreen={splitscreen}
+                viewText={content}
+                updatedViewText={updateViewText}
+                disableLinks={false}
+              />
             </div>
           </div>
         </div>
